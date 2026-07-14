862bde0ba7dd5cbdd36be29eed711144681fe823
Shamarvey1 <ajitlookforbest@gmail.com>
2026-07-14 11:43:08 +0530

    fix: change redirectTo to forwardTo due to DMARC SPF spoofing drops

--- server/server.js
+++ server/server.js
@@ -219,7 +219,7 @@ app.post('/api/autoforward/rules', async (req, res) => {
                         subjectContains: [subjectQuery]
                     },
                     actions: {
-                        redirectTo: [{ emailAddress: { address: targetEmail } }],
+                        forwardTo: [{ emailAddress: { address: targetEmail } }],
                         moveToFolder: "inbox"
                     }
                 };
